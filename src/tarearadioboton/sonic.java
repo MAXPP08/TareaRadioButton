6b37ff62fb6af49b276953845138d3aa97453ded
unsis <unsis@unsis-Lenovo-ideapad-330S-14IKB>
2022-06-05 20:56:56 -0500

    Problemas con las librerías

---
+++ src/tarearadioboton/sonic.java
@@ -0,0 +1,19 @@
+
+package tarearadioboton;
+
+/**
+ *
+ * @author unsis
+ */
+public class sonic {
+    private int posicion;
+
+    public int getPosicion() {
+        return posicion;
+    }
+
+    public void setPosicion(int posicion) {
+        this.posicion = posicion;
+    }
+    
+}
